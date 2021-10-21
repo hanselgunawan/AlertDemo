bc69be6dda6d2134582144d604ffc9f3a6b23fc2
Hansel Tritama <hansel.tritama@nortonlifelock.com>
2021-10-20 22:28:21 -0700

    Open SECURITY SETTINGS screen

--- app/src/main/java/com/hanseltritama/alertdemo/MainActivity.java
+++ app/src/main/java/com/hanseltritama/alertdemo/MainActivity.java
@@ -2,6 +2,10 @@ package com.hanseltritama.alertdemo;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -18,9 +22,16 @@ public class MainActivity extends AppCompatActivity {
                 .setTitle("Are you sure?")
                 .setMessage("Do you definitely want to do this?")
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @RequiresApi(api = Build.VERSION_CODES.R)
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_SHORT).show();
+                        Intent intent =
+                            Build.VERSION.SDK_INT < Build.VERSION_CODES.R ?
+                            new Intent(Settings.ACTION_SECURITY_SETTINGS) :
+                            new Intent(Settings.ACTION_BIOMETRIC_ENROLL);
+
+//                        Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_SHORT).show();
+                        startActivity(intent);
                     }
                 })
                 .setNegativeButton("No", null)
